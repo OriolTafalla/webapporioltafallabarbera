9deb0d4ca124cff07d75d20a250a68ab887812b9
OriolTafallaBarberà <oriol.tafalla@institutroquetes.cat>
2025-04-11 11:57:38 +0200

    Bug fix com Arantxa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let mapa ;
-let seccio_origen ;
+let mapa;
+let seccio_origen;
 let model, webcam, prediccions, maxPrediccions;
 let canvas_creat = false;
 let diagrama;
@@ -8,14 +8,7 @@ let valors = [[],[]];
 function canvia_seccio(num_boto) {
     const menu = document.getElementById("menu");
     const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
-    for (let i = 1; i < num_botons; i++) {
-        let boto = document.getElementById("boto_" + i);
-        let seccio = document.getElementById("seccio_" + i);
-        if (i == num_boto) {
-            boto.style.color = "#17153B";    // es destaca la secció activa amb el canvi de colors del botó corresponent
-            boto.style.backgroundColor = "#F6F0F0";
-            seccio.style.display = "flex";    // es fa visible la secció activa
-        if (num_boto == 3) {    // si es prem el botó de la secció "Galeria"
+     if (num_boto == 3) {    // si es prem el botó de la secció "Galeria"
         omple_llista();
         }
         if (num_boto == 4) {
@@ -26,6 +19,14 @@ function canvia_seccio(num_boto) {
                 if (num_boto == 6) {
         mostra_diagrama();
     }
+    for (let i = 1; i < num_botons; i++) {
+        let boto = document.getElementById("boto_" + i);
+        let seccio = document.getElementById("seccio_" + i);
+        if (i == num_boto) {
+            boto.style.color = "#17153B";    // es destaca la secció activa amb el canvi de colors del botó corresponent
+            boto.style.backgroundColor = "#F6F0F0";
+            seccio.style.display = "flex";    // es fa visible la secció activa
+       
 }
         }
         else {
@@ -102,7 +103,13 @@ function tanca_sessio() {
     }
 }
 //------------------------------------------------------------------------------------------------------------------------
-window.onload = () => { 
+window.onload = () => {
+    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
+    addTo(mapa);
+    }
+    // s'afegeix la capa al mapa
     let base_de_dades = storage.getItem("base_de_dades");   
     if(base_de_dades == null) {
         indexedDB.open("Dades").onupgradeneeded = event => {   
@@ -125,12 +132,7 @@ window.onload = () => {
                 document.getElementById("icona_camera").style.display = "none";    // s'oculta la icona que hi havia abans de fer la foto
                 document.getElementById("desa").style.display = "unset";    // es mostra el botó per desar la foto
             }
-        }
-    });
-    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
-}).addTo(mapa);    // s'afegeix la capa al mapa
+        });
 }
 //------------------------------------------------------------------------------------------------------------------------
 function desa_foto() {
