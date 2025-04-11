8683b04209f5c01d44127e9a2a521c382e91aad7
OriolTafallaBarberà <oriol.tafalla@institutroquetes.cat>
2025-04-11 13:51:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function canvia_seccio(num_boto) {
         let boto = document.getElementById("boto_" + i);
         let seccio = document.getElementById("seccio_" + i);
         if (i == num_boto) {
-            boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
-            boto.style.backgroundColor = "#FCDEE0";
+            boto.style.color = "#17153B";    // es destaca la secció activa amb el canvi de colors del botó corresponent
+            boto.style.backgroundColor = "#F6F0F0";
             seccio.style.display = "flex";    // es fa visible la secció activa
         }
         else {
