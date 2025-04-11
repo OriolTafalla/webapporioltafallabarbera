29d0ff7f2aae0bb3ce8b2addf18395cf7e37c0aa
OriolTafallaBarberà <oriol.tafalla@institutroquetes.cat>
2025-04-11 12:59:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -214,3 +214,85 @@ function geoExit(posicio) {
         alert("No s'ha pogut obtenir la ubicació");
     }
 }
+
+let pixels = 24;    // nombre de píxels de la forma
+let mida = 2 * pixels;    // mida de visualització en el mapa
+let ref_vertical = mida / 2;    // distància vertical des del punt superior de la icona fins al punt de la localització
+let color = "#17153B";
+let path = "M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z";    // cadena de text de la forma
+let cadenaSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ' + pixels + ' ' + pixels + '"><path d="' + path + '" fill="' + color + '" /></svg>';    // construcció de l'element SVG
+let icona = encodeURI("data:image/svg+xml," + cadenaSVG);    // codificació d'espais i caràcters especials per formar una URL vàlida
+let icon = L.icon({    // propietats de la icona
+    iconUrl: icona,    // URL de la forma
+    iconSize: [mida, mida],    // mida de la icona
+    iconAnchor: [mida / 2, ref_vertical]    // distàncies (horitzontal i vertical) des del punt superior esquerre de la icona fins al punt de localització
+}); 
+async function inicia_video() {
+    const codi_model = "aC-EgiGmP"    // substitueix els asteriscs pel codi del model d'IA que vas crear en una activitat anterior
+    const tmURL = "https://teachablemachine.withgoogle.com/models/" + codi_model;
+    const modelURL = tmURL + "/model.json";
+    const metadataURL = tmURL + "/metadata.json";
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPrediccions = model.getTotalClasses();    // nombre de tipus d'imatges per reconèixer
+    webcam = new tmImage.Webcam(300, 300, true);    // posada en marxa de la webcam
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);    // bucle
+    document.getElementById("icona_video").style.display = "none";    // oculta la icona de la càmera de vídeo
+    document.getElementById("coincidencia").style.display = "flex";    // mostra el text amb la predicció de coincidències
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    prediccions = document.getElementById("prediccions");
+    for (let i = 0; i < maxPrediccions; i++) {
+        prediccions.appendChild(document.createElement("div"));    // es crea un contenidor per a la coincidència de cada tipus d'imatge
+    }
+}
+async function loop() {
+    webcam.update();
+    await prediu();
+    window.requestAnimationFrame(loop);
+}
+async function prediu() {
+    const prediccio = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPrediccions; i++) {
+        const classe = prediccio[i].className + ": " + prediccio[i].probability.toFixed(2);    // es conserven dues xifres decimals
+        prediccions.childNodes[i].innerHTML = classe;
+    }
+function mostra_diagrama() {
+    if (!canvas_creat) {    // només si no s'ha creat anteriorment
+        diagrama = new Chart(document.getElementById("diagrama"), {
+            type : 'line',    // tipus de diagrama
+            data : {
+                labels : valors[0],    // etiquetes de l'eix X
+                datasets : [
+                        {
+                            data : valors[1],    // valors mesurats
+                            label : "Nivell de llum",    // títol del diagrama
+                            borderColor : "blue",    // color de la línia
+                        }]
+            },
+        });
+        peticio();    // funció que sol·licita el valor més recent del canal de ThingSpeak
+        setInterval(peticio, 20000);    // es sol·licita un valor cada 20 segons, un interval de temps adient per a l'entorn ThingSpeak
+        canvas_creat = true;
+    } 
+}
+function peticio() {
+    const canal = "2897205";    // s'han de substituir els asteriscs pel codi del canal
+    const camp = "1";    // el camp 1 (nivell de llum)
+    const max_dades = 10;    // nombre de valors que es volen visualitzar simultàniament
+    const ts_url = "https://api.thingspeak.com/channels/" + canal + "/fields/" + camp + "/last.json"    // url que sol·licita el valor més recent
+    fetch(ts_url)
+        .then(resposta => resposta.json())
+        .then(resposta => {
+            let valor = Number(resposta["field1"]);    // converteix el tipus de valor rebut (text) en nombre.
+            document.getElementById("div_valor").innerHTML = valor;
+            if (valors[0].length >= max_dades) {
+                valors[0].shift();    // elimina el primer valor de la llista d'etiquetes
+                valors[1].shift();    // elimina el primer valor de la llista de valors
+            }
+            valors[0].push(new Date().toLocaleTimeString());    // afegeix l'hora actual a la llista d'etiquetes
+            valors[1].push(valor);    // afegeix el valor rebut a la llista de valors
+            diagrama.update();    // actualitza el diagrama d'acord amb el valor rebut
+        });
+}
+}
