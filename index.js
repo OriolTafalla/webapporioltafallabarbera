9fd9ed43c60ea2c539fd22de5dc0c3a9f678b798
OriolTafallaBarberà <oriol.tafalla@institutroquetes.cat>
2025-05-25 15:02:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function tanca_sessio() {
     if (validat) {
         if (confirm("Vols tancar la sessió?")) {    // S'ha respost "Sí"
             storage.setItem("usuari", "");
-            location.reload();    // recàrrega de la pàgina, es reinicialitzen totes les variables
+            location.reload(true);    // recàrrega de la pàgina, es reinicialitzen totes les variables
         }
     }
 }
